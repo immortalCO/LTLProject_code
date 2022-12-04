ebbad78f44c0f298f4bfd96eb6248cde2909e621
Junkun Chen <immortalco233@gmail.com>
2022-12-04 05:57:28 -0600

    1

--- train.py
+++ train.py
@@ -302,7 +302,7 @@ def read_data_ns(DATASET, config, i, debug=False):
     img = img * opa.unsqueeze(-1) + torch.tensor(BG_COLOR) * (1 - opa.unsqueeze(-1))
 
     cam_int[:2] = cam_int[:2] / 4
-    proj = cam_int @ cam_ext
+    proj = cam_int @ cam_ext[:3, :4]
 
     return cam, proj, img, opa > opa_thres
 
