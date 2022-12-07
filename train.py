a3b2cd304651616de336bd5ef816e14563095f8a
Junkun Chen <immortalco233@gmail.com>
2022-12-07 04:42:02 -0600

    1

--- train.py
+++ train.py
@@ -568,7 +568,7 @@ def maml_valid_step(mvsnet_orig, episode, num_epoch=30, batch_size=2, alpha=0.02
 
     return test_psnr
 
-def maml_train(mvsnet, episodes, valid_episodes, batch_size=2, lr=0.0025, alpha=0.005, epochs=200):
+def maml_train(mvsnet, episodes, valid_episodes, batch_size=2, lr=0.005, alpha=0.01, epochs=200):
     opt = torch.optim.Adam(mvsnet.parameters(), lr=lr)
     sch = torch.optim.lr_scheduler.StepLR(opt, step_size=20, gamma=0.75)
 
