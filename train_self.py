07508260fa99a2ce752361f8fea698b156626c48
Junkun Chen <immortalco233@gmail.com>
2022-12-08 20:00:38 -0600

    1

--- train_self.py
+++ train_self.py
@@ -545,17 +545,18 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
     grad_updated_param = []
     for param in mvsnet.parameters():
         grad = param.grad
-        if grad is None:
-            grad = torch.zeros_like(param)
-        grad_updated_param.append(grad.detach().clone())
+        if grad is not None:
+            grad = grad.detach().clone()
+        grad_updated_param.append(grad)
 
     for param, grad in zip(mvsnet_orig.parameters(), grad_updated_param):
         # 1st-order gradient update
-        with torch.no_grad():
-            if param.grad is None:
-                param.grad = grad.clone()
-            else:
-                param.grad += grad
+        if grad is not None:
+            with torch.no_grad():
+                if param.grad is None:
+                    param.grad = grad.clone()
+                else:
+                    param.grad += grad
 
     
     # 2nd run: obtain parameters with 2nd order gradient
@@ -566,7 +567,7 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
     mvsnet.eval()
     mvsnet.zero_grad()
 
-    grad_passing_raw = [(grad * -alpha) for grad in grad_updated_param]
+    grad_passing_raw = [(grad * -alpha) if grad is not None else None for grad in grad_updated_param]
     del grad_updated_param
 
     for epoch in range(num_epoch):
