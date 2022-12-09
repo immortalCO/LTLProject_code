6489748fe562174c7102446e2b2149e04f93d91b
Junkun Chen <immortalco233@gmail.com>
2022-12-08 20:17:49 -0600

    1

--- train_self.py
+++ train_self.py
@@ -543,11 +543,14 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
         loss.backward()
 
     grad_updated_param = []
-    for param in mvsnet.parameters():
+    grad_passing_raw = []
+    for name, param in mvsnet.named_parameters():
         grad = param.grad
         if grad is not None:
             grad = grad.detach().clone()
         grad_updated_param.append(grad)
+        if name.startswith("loss_net"):
+            grad_passing_raw.append(grad)
 
     for param, grad in zip(mvsnet_orig.parameters(), grad_updated_param):
         # 1st-order gradient update
@@ -567,19 +570,21 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
     mvsnet.eval()
     mvsnet.zero_grad()
 
-    grad_passing_raw = [(grad * -alpha) if grad is not None else None for grad in grad_updated_param]
-    del grad_updated_param
-
     for epoch in range(num_epoch):
         mvsnet.zero_grad()
         for (batch_cams, batch_imgs, _, _) in train_loader:
             loss = mvsnet(batch_imgs, batch_cams, training=True)
             loss = loss * batch_imgs.shape[0] / len(episode)
 
+            loss_net_params = []
+            for name, param in mvsnet.named_parameters():
+                if name.startswith("loss_net"):
+                    loss_net_params.append(param)
+
             update_raw = torch.autograd.grad(
-                loss, mvsnet.parameters(), 
+                loss, loss_net_params, 
                 create_graph=True, retain_graph=False, allow_unused=True)
-                
+
             update = []
             grad_passing = []
             for ug, pg in zip(update_raw, grad_passing_raw):
@@ -588,10 +593,10 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
                     grad_passing.append(pg)
             
             grad_contribute = torch.autograd.grad(
-                update, mvsnet.parameters(), grad_passing, 
+                update, loss_net_params, grad_passing, 
                 create_graph=False, retain_graph=False, allow_unused=True)
 
-            for param, grad in zip(mvsnet_orig.parameters(), grad_contribute):
+            for param, grad in zip(mvsnet_orig.loss_net.parameters(), grad_contribute):
                 # 2nd-order gradient update
                 if grad is not None:
                     with torch.no_grad():
