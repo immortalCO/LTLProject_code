2f3c45a850a9270008bf73d2b7bd150d71e3f215
Junkun Chen <immortalco233@gmail.com>
2022-12-08 23:48:07 -0600

    1

--- train_self.py
+++ train_self.py
@@ -509,7 +509,9 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
     import copy
     mvsnet = copy.deepcopy(mvsnet_orig)
     mvsnet.zero_grad()
-    opt = torch.optim.Adam(mvsnet.parameters(), lr=alpha)
+    for param in mvsnet.loss_net.parameters():
+        param.requires_grad = False
+    opt = torch.optim.Adam(mvsnet.mvsnet.parameters(), lr=alpha)
     sch = torch.optim.lr_scheduler.StepLR(opt, step_size=5, gamma=0.5)
 
     episode = episode.sample_subset(batch_size * num_batches)
@@ -543,13 +545,13 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
         loss.backward()
 
     grad_updated_param = []
-    for param in mvsnet.parameters():
+    for param in mvsnet.mvsnet.parameters():
         grad = param.grad
         if grad is not None:
             grad = grad.detach().clone()
         grad_updated_param.append(grad)
 
-    for param, grad in zip(mvsnet_orig.parameters(), grad_updated_param):
+    for param, grad in zip(mvsnet_orig.mvsnet.parameters(), grad_updated_param):
         # 1st-order gradient update
         if grad is not None:
             with torch.no_grad():
@@ -577,7 +579,7 @@ def maml_train_step(mvsnet_orig, episode, num_epoch=1, batch_size=2, num_batches
             loss = loss * batch_imgs.shape[0] / len(episode)
 
             update_raw = torch.autograd.grad(
-                loss, mvsnet.parameters(), create_graph=True, allow_unused=True)
+                loss, mvsnet.mvsnet.parameters(), create_graph=True, allow_unused=True)
                 
             update = []
             grad_passing = []
