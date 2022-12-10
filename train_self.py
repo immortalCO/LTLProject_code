6a93d85bd64b14c544dd68cccf01d23caafb23be
Junkun Chen <immortalco233@gmail.com>
2022-12-09 19:00:01 -0600

    1

--- train_self.py
+++ train_self.py
@@ -674,7 +674,7 @@ def maml_valid_step(mvsnet_orig, episode, num_epoch=40, batch_size=2, alpha=0.00
 
 def maml_train(mvsnet, episodes, valid_episodes, save_ckpt, 
         batch_size=2, lr=0.001, alpha=0.001, epoch_fact=100):
-    assert isinstance(mvsnet, MVSNetSelfSup), "Should be self-supervised MVSNet"
+    # assert isinstance(mvsnet, MVSNetSelfSup), "Should be self-supervised MVSNet"
     epochs = epoch_fact * 10
     opt = torch.optim.Adam(mvsnet.parameters(), lr=lr)
     sch = torch.optim.lr_scheduler.StepLR(opt, step_size=epoch_fact, gamma=0.75)
